e92249f616329cd0eee473173ac7a57c3f91ddeb
JinHao <hao.jin@oracle.com>
2016-09-17 18:54:13 +0800

    for methods with multiple parameters with default values, the restriction of using default values for trailing parameters is removed

--- DefaultValues.scala
+++ DefaultValues.scala
@@ -1,8 +1,9 @@
 def mail(destination: String = "head office", mailClass: String = "first") =
-  println(s"sending to $destination by $mailClass class")
+	println(s"sending to $destination by $mailClass class")
 
 
 mail("Houston office", "Priority")
 mail("Boston office")
 mail()
 mail(mailClass = "Priority", destination = "LA")
+mail(mailClass = "Restriction")
