8ab79a302528d0a452fa4f83407aa1850e7f1660
JinHao <hao.jin@oracle.com>
2016-09-17 16:17:30 +0800

    try Named parameters

---
+++ DefaultValues.scala
@@ -0,0 +1,8 @@
+def mail(destination: String = "head office", mailClass: String = "first") =
+  println(s"sending to $destination by $mailClass class")
+
+
+mail("Houston office", "Priority")
+mail("Boston office")
+mail()
+mail(mailClass = "Priority", destination = "LA")
